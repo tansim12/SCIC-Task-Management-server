01b8c53c8f6a30f71b80e7f83b2b2ead7257afbc
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-12-22 05:02:43 +0600

    backend is all done here

--- src/api/To Do method/getAllTodo.js
+++ src/api/To Do method/getAllTodo.js
@@ -1,19 +1,18 @@
-const Todo = require("../../model/To Do Model/todoModel")
+const Todo = require("../../model/To Do Model/todoModel");
 
-const getAllTodo=async (req, res)=>{
-    const email = req.query?.email
-   
-    
-    let queryObj = {}
-    if (email) {
-        queryObj.email=email
-    }
-   
-    const result = await Todo.find(queryObj)
-    const todoData = result.filter(item=> item?.category === "Todo")
-    const ongoingData = result.filter(item=> item?.category === "Ongoing")
-    const completedData = result.filter(item=> item?.category === "Completed")
-    
-    res.send({todoData,ongoingData,completedData})
-}
-module.exports=getAllTodo
+const getAllTodo = async (req, res) => {
+  const email = req.query?.email;
+
+  let queryObj = {};
+  if (email) {
+    queryObj.email = email;
+  }
+
+  const result = await Todo.find(queryObj);
+  const todoData = result.filter((item) => item?.category === "Todo");
+  const ongoingData = result.filter((item) => item?.category === "Ongoing");
+  const completedData = result.filter((item) => item?.category === "Completed");
+
+  res.send({ todoData, ongoingData, completedData });
+};
+module.exports = getAllTodo;
