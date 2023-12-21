b37a9e87d44edc5e9ac7fcb01d8952d8e622a1fd
TANSIM AHMED TASHDID <tansimahmedtasjid@gmail.com>
2023-12-21 18:09:43 +0600

    todo update section is done here

---
+++ src/api/To Do method/updateTodo.js
@@ -0,0 +1,12 @@
+const Todo = require("../../model/To Do Model/todoModel");
+const { deleteOne } = require("../../model/To Do Model/todoModel");
+
+const updateTodo = async (req, res) => {
+  const id = req.params._id;
+  const info = req.body;
+  const result = await Todo.updateOne({ _id: id }, { ...info });
+  if (result?.acknowledged) {
+    res.send({acknowledged});
+  }
+};
+module.exports = updateTodo;
--- src/route/To do/index.js
+++ src/route/To do/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const postTodos = require('../../api/To Do method/postTodos');
 const getAllTodo = require('../../api/To Do method/getAllTodo');
+const updateTodo = require('../../api/To Do method/updateTodo');
 const router = express.Router()
 
 router.post('/todo', postTodos )
 router.get('/todo', getAllTodo )
+router.put('/todo/:_id', updateTodo )
 module.exports=router
